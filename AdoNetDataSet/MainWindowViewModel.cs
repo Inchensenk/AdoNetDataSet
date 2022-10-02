eee345b3b040d532e483f817352f382192a8d307
sofoyan gevork <aab81@bk.ru>
2022-10-03 01:14:44 +0300

    Добавьте файлы проекта.

---
+++ AdoNetDataSet/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDataSet
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public DelegateCommand ClickCommand { get; } = null!;
+        public ObservableCollection<Person> People
+        {
+            get;
+            set;
+        }
+        public MainWindowViewModel()
+        {
+            ClickCommand = new(OnClickAsync);
+
+            People = new();
+        }
+
+        private /*async*/ void OnClickAsync()
+        {
+            string connectionString = "Server=PIXEL; Database=AdoExplanationDb; Trusted_Connection=True; Encrypt=false";
+
+            string sql = "SELECT * FROM Persons";
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                SqlDataAdapter dataAdapter = new(sql, connection);
+
+                DataSet dataSet = new();
+
+                dataAdapter.Fill(dataSet);
+
+                var table = dataSet.Tables[0];
+
+                foreach (DataRow row in table.Rows)
+                {
+                    var person = new Person();
+
+                    person.Name = row["Name"] as string;
+
+                    var age = row["Age"] as int?;
+                    person.Age = age.HasValue ? age.Value : 0;
+
+                    var height = row["Age"] as float?;
+                    person.Height = height.HasValue ? height.Value : 0;
+
+                    People.Add(person);
+                }
+               /* //ожидает в текущем потоке пока выполнится другой поток
+                await connection.OpenAsync();*/
+            }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AdoNetDataSet/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDataSet
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        private int _age;
+
+        public int Id
+        {
+            get;
+
+            set;
+        }
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+
+            set
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public double Height { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
